811748723ca35679c67e2566c1bc0b0bf0ffe1ff
sadraabadkar <131643772+sadragit1383@users.noreply.github.com>
2024-11-09 08:48:29 +0330

    Update views.py
    new_change

--- shop/apps/account/views.py
+++ shop/apps/account/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from rest_framework import status, permissions
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -16,46 +16,20 @@ from .serializers import (
 )
 
 
-class CustomerUserView(APIView):
+
+class CustomerUserView(generics.ListCreateAPIView):
     """View for listing and creating CustomUser instances."""
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAdminUser]
     authentication_classes = [JWTAuthentication]  # Use JWT authentication
 
-    def get(self, request):
-        users = CustomUser.objects.all()
-        serializer = CustomUserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CustomUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CustomUserViewDetail(APIView):
+class CustomUserViewDetail(generics.RetrieveUpdateDestroyAPIView):
     """View for retrieving, updating, and deleting CustomUser instances."""
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
-
-    def get(self, request, pk):
-        user = get_object_or_404(CustomUser, pk=pk)
-        serializer = CustomUserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        user = get_object_or_404(CustomUser, pk=pk)
-        serializer = CustomUserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        user = get_object_or_404(CustomUser, pk=pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    authentication_classes = [JWTAuthentication]  # Use JWT authentication
 
 
 class RegisterUserView(APIView):
